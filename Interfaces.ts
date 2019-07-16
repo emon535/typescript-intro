b7fd5904dd5738cb6a7272dd1f6442d00ad32c2c
Foysal Ahmed Emon <emon535@gmail.com>
2019-07-16 22:28:41 +0600

    Interface

---
+++ Interfaces.ts
@@ -0,0 +1,13 @@
+interface Point {
+  x: number;
+  y: number;
+  draw: () => void;
+}
+
+let drawPointwithInterface = (point: Point) => {
+  //DrawPointerWithInterface
+};
+
+let getDistance = (pointA: Point, pointB: Point) => {
+  //DrawPointerWithInterface
+};
