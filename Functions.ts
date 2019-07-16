1999ae05056cf6bae9a1d212a1c320e24a38bf57
Foysal Ahmed Emon <emon535@gmail.com>
2019-07-16 22:25:00 +0600

    Functions

---
+++ Functions.ts
@@ -0,0 +1,15 @@
+let mylog = function(message) {
+  console.log(message);
+};
+
+let doLog = message => console.log(message);
+let doLogwithEmpty = () => console.log(message);
+
+let drawPoint = (point: { x: number; y: number }) => {
+  //draw
+};
+
+drawPoint({
+  x: 1,
+  y: 23
+});
